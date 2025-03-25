df4db9e36c3290748d16899e0212ab60c6673488
Yui San ae2f <mincraft20046@gmail.com>
2025-03-26 03:13:33 +0900

    Update Core.cmake

--- cmake/Core.cmake
+++ cmake/Core.cmake
@@ -1,7 +1,7 @@
 option(ae2f_IS_SHARED "Is a shared library or static one." OFF)
 option(ae2f_DOC "When activated, it would generate project with the deaders of cmake utility functions." OFF)
 option(ae2f_TEST "When activated, it would generate test projects." ON)
-option(ae2f_CXX "Tell that thou art including cxx for thy project." OFF)
+option(ae2f_CXX "Tell that thou art including cxx for thy project." ON)
 
 set(ae2f_float float CACHE STRING "Float type for the template.")
 set(ae2f_packcount 0 CACHE STRING "Pack count for pre-defined structures.")
@@ -181,7 +181,7 @@ endfunction()
 # 
 # Once the project is in given directory, it will not try to fetch it from internet.
 # 
-# Fethced Library will be in: ${prm_AuthorName}__${prm_TarName}__FETCHED 
+# Fetched library will be in ${prm_AuthorName}__${prm_namespace}__${prm_TarName}__FETCHED 
 # 
 # @param prm_AuthorName 
 # Author name
@@ -189,39 +189,42 @@ endfunction()
 # Target name 
 # @param prm_TagName
 # Tag name
-function(ae2f_CoreLibFetch prm_AuthorName prm_TarName prm_TagName)
-    if(NOT TARGET ${prm_TarName})
-	    if(NOT EXISTS ${ae2f_ProjRoot}/submod/${prm_AuthorName}/${prm_TarName}/CMakeLists.txt)
-	        execute_process(
-	            COMMAND 
-	            git clone 
-	            https://github.com/${prm_AuthorName}/${prm_TarName} 
-	            ${ae2f_ProjRoot}/submod/${prm_AuthorName}/${prm_TarName}
-	            --branch ${prm_TagName}
-	            RESULT_VARIABLE result
-	        )
-	
-	        if(result)
-	            message(FATAL_ERROR "Fetching ${prm_AuthorName}/${prm_TarName} from Github Failed.")
-	        endif()
-	    endif()
-    
-		add_subdirectory(
-		    ${ae2f_ProjRoot}/submod/${prm_AuthorName}/${prm_TarName}
-		    ${ae2f_BinRoot}/submod/${prm_AuthorName}/${prm_TarName}
-		)
+function(ae2f_CoreLibFetch prm_AuthorName prm_namespace prm_TarName prm_TagName)
+    if(NOT EXISTS ${ae2f_ProjRoot}/submod/${prm_AuthorName}/${prm_TarName}/CMakeLists.txt)
+        execute_process(
+            COMMAND 
+            git clone 
+            https://github.com/${prm_AuthorName}/${prm_TarName} 
+            ${ae2f_ProjRoot}/submod/${prm_AuthorName}/${prm_TarName}
+            --branch ${prm_TagName}
+            RESULT_VARIABLE result
+        )
+
+        if(result)
+            message(FATAL_ERROR "Fetching ${prm_AuthorName}/${prm_TarName} from Github Failed.")
+        endif()
     endif()
 
-    set(${prm_AuthorName}__${prm_TarName}__FETCHED ${prm_TarName} CACHE STRING ${prm_TarName})
+    if(NOT TARGET ${prm_TarName})
+        add_subdirectory(
+            ${ae2f_ProjRoot}/submod/${prm_AuthorName}/${prm_TarName}
+            ${ae2f_BinRoot}/submod/${prm_AuthorName}/${prm_TarName}
+        )
+    endif()
+
+    set(${prm_AuthorName}__${prm_namespace}__${prm_TarName}__FETCHED ${prm_TarName})
 endfunction()
 
 
-# Fetched library will be in ${prm_AuthorName}__${prm_TarName}__FETCHED 
-function(ae2f_CoreLibFetchX prm_AuthorName prm_TarName prm_TagName)
+# Fetched library will be in ${prm_AuthorName}__${prm_namespace}__${prm_TarName}__FETCHED 
+function(ae2f_CoreLibFetchX prm_AuthorName prm_namespace prm_TarName prm_TagName)
 	find_package(${prm_TarName})
 
 	if(${prm_TarName}_FOUND)
-		set(${prm_AuthorName}__${prm_TarName}__FETCHED ${prm_AuthorName}::${prm_TarName} CACHE STRING ${prm_AuthorName}::${prm_TarName})
+		set(
+            ${prm_AuthorName}__${prm_namespace}__${prm_TarName}__FETCHED
+            ${prm_namespace}::${prm_TarName}
+            )
 	else()
 		ae2f_CoreLibFetch(${prm_AuthorName} ${prm_TarName} ${prm_TagName})
 	endif()
